66e2154fdebf44e29873e6c5eeaba4014a4dc8e0
chihyanghsu0805 <chihyanghsu0805@gmail.com>
2020-02-27 14:22:10 -0600

    Add files via upload

--- scale_dose.m
+++ scale_dose.m
@@ -1,22 +1,20 @@
-function scale_dose(dicom_list, output_list)
+function scale_dose(dicom_path, output_path)
 
-num_subjects = size(dicom_list, 1);
-for i = 1:num_subjects
-    
-    dicom_path = dicom_list{i};
-    dose = dicomread(dicom_path);
-    info = dicominfo(dicom_path);
-    dose = double(dose);
-    dose = dose*info.DoseGridScaling;
-    dose = dose*100;
-    dose = uint16(dose);
-    info.DoseGridScaling = 1;
-    info.SeriesInstanceUID = dicomuid;
-    
-    output_path = output_list{i};
+dicom_path = dicom_path;
+dose = dicomread(dicom_path);
+info = dicominfo(dicom_path);
+dose = double(dose);
+dose = dose*info.DoseGridScaling;
+dose = dose*100;
+dose = uint16(dose);
+info.DoseGridScaling = 1;
+info.SeriesInstanceUID = dicomuid;
+
+if ~isempty(output_path)
     if ~exist(output_path, 'dir')
         mkdir(output_path)
     end
     dicomwrite(dose, output_path, info, 'CreateMode', 'copy')
-    
 end
+
+
